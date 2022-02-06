c06cbbcb80bb2c90c19a3319d36e4191e60c8d9c
DESKTOP-QUST204\nqwrt <nqwrt@ymail.com>
2022-02-06 22:34:47 +0900

    commit 2

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default axios.create({
-  baseURL: "http://localhost:8282",
+  baseURL: 'http://146.56.137.240:8282/hjs',
   headers: {
-    "Content-type": "application/json"
-  }
+    'Content-type': 'application/json',
+  },
 });
